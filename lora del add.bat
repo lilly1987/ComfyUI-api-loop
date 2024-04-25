8b30949fbb30f0c08db0a377fb8d06a548366ada
lilly <lilly1987@naver.com>
2024-04-25 19:31:16 +0900

    1

--- lora del add.bat
+++ lora del add.bat
@@ -1,5 +1,10 @@
 :REDO
 ..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/checkpoints/2d" "U:/models/checkpoints/2d"
+..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/checkpoints/pony" "U:/models/checkpoints/pony"
+..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/checkpoints/xl" "U:/models/checkpoints/xl"
+..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/loras/XL" "U:/models/loras/XL"
+..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/loras/pony" "U:/models/loras/pony"
+..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/loras/step" "U:/models/loras/step"
 ..\python_embeded\python.exe -s  "lora del add.py" "../ComfyUI/models/loras" "U:/models/loras"
 rem ..\python_embeded\python.exe -s  "lora del add.py" %*
 rem ..\python_embeded\python.exe -s  basic_api_example.py %*
